a2d962c2e811d1c966a8b7d0c85742815fe68bfe
luiz <duduzinho310310@gmail.com>
2026-06-15 23:20:45 -0300

    Update banco.py

--- Backend/principais/banco.py
+++ Backend/principais/banco.py
@@ -3,12 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import sqlite3
 from datetime import datetime
-from Backend.principais.mya import perguntar_mya
-from Backend.principais.analise_financeira import analisar_usuario 
-from Backend.principais.analise_financeira import categorias_principais
-from Backend.secundarios.notify import criar_notificacao
-from Backend.secundarios.scheduler import scheduler
-from Backend.principais.analise_financeira import gerar_dicas_economia
+from principais.mya import perguntar_mya
+from principais.analise_financeira import analisar_usuario 
+from principais.analise_financeira import categorias_principais
+from secundarios.notify import criar_notificacao
+from secundarios.scheduler import scheduler
+from principais.analise_financeira import gerar_dicas_economia
 
 app = FastAPI()
 
