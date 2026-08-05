5d1fa0b8308e725a779b875351f28d6e996d9ef5
luiscript-ed <duduzinho310310@gmail.com>
2026-08-05 12:28:24 -0300

    Tirando a pha dos cookies

--- Index_addons/script.js
+++ Index_addons/script.js
@@ -4,19 +4,6 @@ console.log("YOFI inicializado com sucesso!");
 
 const cards = document.querySelectorAll(".feature-card");
 
-// Biblioteca de cookie
-import Cookies from "js-cookie";
-
-// Sistema de Login automatico, para facilitar a vida dos usuários
-// Criação do cook e seu tempo até a pessoa ter que fazer outro login
-const fez_login = Cookies.set()("login", "true",{
-    expires: 30 
-        });
-
-// Verificação do login
-if (Cookies.get("login") === "true") {
-    window.open("https://luiscript-ed.github.io/YOFI/Front-end/Inicial/page", "_blank");;
-}
 // Sistema ultra passado de aumentar o tamanho dos itens pelo js
 cards.forEach(card => {
 
